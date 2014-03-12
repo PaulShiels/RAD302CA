0bb5c14cab31891e31208ae7728f4bc8b31dd5b1
PaulShiels <pshielss@gmail.com>
2014-03-12 14:50:54 +0000

    Finally loading

---
+++ Trip_booking/Trip_booking/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trip_booking.DAL;
+
+namespace Trip_booking.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ITripRepository _repo;
+
+        public HomeController(ITripRepository repo)
+        {
+            _repo = repo;
+        }
+        public ActionResult Index()
+        {
+            //int i = 0;
+            //i = _repo.GetAllTrips().Count();
+            return View("Trips", _repo.GetAllTrips());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
